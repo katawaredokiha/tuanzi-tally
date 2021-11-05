3b95cba4f529fb87f1d29036821f682a2a2836e9
Mike <1079537183@qq.com>
2021-11-05 11:12:41 +0800

    创建 <Layout />

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      Money.vue
-    </div>
-    <Nav/>
-  </div>
+    <Layout>
+      你好
+    </Layout>
 </template>
 
 <script lang="ts">
@@ -12,16 +9,3 @@ export default {
   name: 'Money',
 };
 </script>
-
-<style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content {
-  overflow: auto;
-  flex-grow: 1;
-}
-</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      Statistics.vue
-    </div>
-    <Nav/>
-  </div>
+  <Layout>
+    Statistics.vue
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -12,16 +9,3 @@ export default {
   name: 'Statistics',
 };
 </script>
-
-<style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content {
-  overflow: auto;
-  flex-grow: 1;
-}
-</style>
