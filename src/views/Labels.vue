20ab0a21539f14244a7fc269d014a02aae837a7f
Mike <1079537183@qq.com>
2021-11-09 16:50:38 +0800

    将 tag 的 string 改为 {id, name}

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,8 +1,8 @@
 <template>
   <Layout>
     <ol class="tags">
-      <li v-for="tag in tags" :key="tag">
-        <span>{{tag}}</span>
+      <li v-for="tag in tags" :key="tag.id">
+        <span>{{tag.name}}</span>
         <Icon name="right"/>
       </li>
     </ol>
@@ -17,9 +17,11 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import tagListModel from '@/models/tagListModel';
 
+tagListModel.fetch();
+
 @Component
 export default class Labels extends Vue {
-  tags = tagListModel.fetch();
+tags = tagListModel.data
 
   createTag(){
     const name =window.prompt('请输入标签名')
