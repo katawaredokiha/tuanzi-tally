3c94b1d157dde092e63c84d039891c6f4deb6a80
Mike <1079537183@qq.com>
2021-11-12 11:30:55 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -17,15 +17,18 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 
 @Component({
-  computed:{
+  computed: {
     tagList() {
-      // return this.$store.fetchTags();
-      return []
+      return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends Vue {
   selectedTags: string[] = [];
+
+  created() {
+    this.$store.commit('fetchTags');
+  }
 
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
@@ -40,7 +43,7 @@ export default class Tags extends Vue {
   create() {
     const name = window.prompt('请输入标签名');
     if (!name) { return window.alert('标签名不能为空'); }
-    // store.createTag(name);
+    this.$store.commit('createTag', name);
   }
 }
 </script>
