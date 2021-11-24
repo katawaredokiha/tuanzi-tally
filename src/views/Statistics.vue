1b4abf6768b1a50c9b73c26fa46f08900d517e20
Mike <1079537183@qq.com>
2021-11-24 10:58:55 +0800

    自动更新 options

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -72,11 +72,11 @@ export default class Statistics extends Vue {
       // this.recordList = [{date:7.3, value:100}, {date:7.2, value:200}]
       const dateString = day(today)
           .subtract(i, 'day').format('YYYY-MM-DD');
-      const found = _.find(this.recordList, {
-        createdAt: dateString
+      const found = _.find(this.groupedList, {
+        title: dateString
       });
       array.push({
-        key: dateString, value: found ? found.amount : 0
+        key: dateString, value: found ? found.total : 0
       });
     }
     // 对日期进行排序
