b3497cf0822d2c7db0087435fedbac83082e0eab
Mike <1079537183@qq.com>
2021-11-24 10:31:34 +0800

    构造30天的数据（借鉴计数排序）

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -31,6 +31,8 @@ import recordTypeList from '@/constants/typeList';
 import dayjs from 'dayjs';
 import clone from '@/lib/clone';
 import Chart from '@/components/Chart.vue';
+import _ from 'lodash';
+import day from 'dayjs';
 
 @Component({
   components: {Tabs, Chart},
@@ -63,7 +65,36 @@ export default class Statistics extends Vue {
     }
   }
 
+  get y() {
+    const today = new Date();
+    const array = [];
+    for (let i = 0; i <= 29; i++) {
+      // this.recordList = [{date:7.3, value:100}, {date:7.2, value:200}]
+      const dateString = day(today)
+          .subtract(i, 'day').format('YYYY-MM-DD');
+      const found = _.find(this.recordList, {
+        createdAt: dateString
+      });
+      array.push({
+        date: dateString, value: found ? found.amount : 0
+      });
+    }
+    // 对日期进行排序
+    array.sort((a, b) => {
+      if (a.date > b.date) {
+        return 1;
+      } else if (a.date === b.date) {
+        return 0;
+      } else {
+        return -1;
+      }
+    });
+    return array;
+  }
+
   get x() {
+    const keys = this.y.map(item => item.date);
+    const values = this.y.map(item => item.value);
     return {
       grid: {
         left: 0,
@@ -71,11 +102,7 @@ export default class Statistics extends Vue {
       },
       xAxis: {
         type: 'category',
-        data: [
-          '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-          '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
-          '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-        ],
+        data: keys,
         axisTick: {alignWithLabel: true},
         axisLine: {lineStyle: {color: '#666'}}
       },
@@ -88,12 +115,7 @@ export default class Statistics extends Vue {
         symbolSize: 12,
         itemStyle: {borderWidth: 1, color: '#666', borderColor: '#666'},
         // lineStyle: {width: 10},
-        data: [
-          820, 932, 901, 934, 1290, 1330, 1320,
-          820, 932, 901, 934, 1290, 1330, 1320,
-          820, 932, 901, 934, 1290, 1330, 1320,
-          820, 932, 901, 934, 1290, 1330, 1320, 1, 2
-        ],
+        data: values,
         type: 'line'
       }],
       tooltip: {
