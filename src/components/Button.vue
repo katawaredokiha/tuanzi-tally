b164147042e354de7350d859144bfcb837ca07eb
Mike <1079537183@qq.com>
2021-11-26 10:28:08 +0800

    修改 Button 样式

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -15,12 +15,12 @@ export default class Button extends Vue {
 
 <style lang="scss" scoped>
 .button {
-  background: #767676;
+  background: #26b2fc;
   color: white;
   border-radius: 4px;
   border: none;
-  height: 40px;
-  padding: 0 16px;
+  height: 32px;
+  padding: 0 8px;
   &-wrapper {
     text-align: center;
     padding: 16px;
