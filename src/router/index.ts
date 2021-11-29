6f4f73b690dcb9c83b4fe9cbb743b0e496657ea0
Mike <1079537183@qq.com>
2021-11-29 10:13:33 +0800

    统计组件重命名为 account

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import VueRouter, {RouteConfig} from 'vue-router';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
-import Statistics from '@/views/Statistics.vue';
+import Statistics from '@/views/account.vue';
 import CreateTags from '@/views/CreateTags.vue';
 import EditLabel from '@/views/EditLabel.vue';
 import NotFound from '@/views/NotFound.vue';
