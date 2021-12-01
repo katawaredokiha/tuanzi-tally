55374d2ea5b33d02b02d95d49bf88eabb6fb846b
Mike <1079537183@qq.com>
2021-12-01 19:50:45 +0800

    完成 CreateTags.vue 样式和功能

--- src/views/CreateTags.vue
+++ src/views/CreateTags.vue
@@ -9,58 +9,6 @@
                 placeholder="请输入标签名"
                 :value.sync="tag.name"/>
     </div>
-    <div class="tagsIcon">
-      <div class="title">
-        图标
-      </div>
-      <ul class="newTagIcons">
-        <li active-class="selected">
-          <div class="icon-wrapper">
-            <Icon name="add"/>
-          </div>
-        </li>
-        <li>
-          <div class="icon-wrapper">
-            <Icon name="add"/>
-          </div>
-        </li>
-        <li>
-          <div class="icon-wrapper">
-            <Icon name="add"/>
-          </div>
-        </li>
-        <li>
-          <div class="icon-wrapper">
-            <Icon name="add"/>
-          </div>
-        </li>
-        <li>
-          <div class="icon-wrapper">
-            <Icon name="add"/>
-          </div>
-        </li>
-        <li>
-          <div class="icon-wrapper">
-            <Icon name="add"/>
-          </div>
-        </li>
-        <li>
-          <div class="icon-wrapper">
-            <Icon name="add"/>
-          </div>
-        </li>
-        <li>
-          <div class="icon-wrapper">
-            <Icon name="add"/>
-          </div>
-        </li>
-        <li>
-          <div class="icon-wrapper">
-            <Icon name="add"/>
-          </div>
-        </li>
-      </ul>
-    </div>
     <div class="button-wrapper">
       <Button @click="createTag">确认</Button>
     </div>
@@ -109,64 +57,18 @@ export default class createTags extends Vue {
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
-$bg: #f8f8f8;
-$h: 24px;
 header {
   height: 48px;
   line-height: 48px;
   text-align: center;
   font-weight: 700;
+  background: #f5f5f5;
 }
 .newTagName {
   padding: 8px 0;
   @extend %innerShadow;
-}
-.tagsIcon {
-  width: 100%;
-  .title {
-    height: 48px;
-    line-height: 48px;
-    padding: 0 16px;
-    font-size: 14px;
-    @extend %innerShadow-bottom
-  }
-  .newTagIcons {
-    height: 50vh;
-    background: white;
-    padding: 16px;
-    display: flex;
-    flex-grow: 1;
-    justify-content: start;
-    align-content: flex-start;
-    overflow-y: auto;
-    flex-wrap: wrap;
-    > li {
-      padding: 4px 0;
-      margin-bottom: 4px;
-      width: 25%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      .icon-wrapper {
-        background: $bg;
-        border-radius: 20px;
-        padding: 8px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        .icon {
-          width: $h;
-          height: $h;
-          font-size: $h;
-        }
-      }
-      &.selected {
-        .icon-wrapper {
-          background: #f69604;
-          color: white;
-        }
-      }
-    }
+  ::v-deep .name {
+    color: #f69604;
   }
 }
 .button-wrapper {
