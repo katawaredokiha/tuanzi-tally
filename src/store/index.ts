3a493596dd571829ac5243bbe5a28c43c2fb4b68
Mike <1079537183@qq.com>
2022-03-18 22:38:13 +0800

    使用 axios 请求接口获取 tagList

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import clone from '@/lib/clone';
 import createId from '@/lib/createId';
+import axios from 'axios';
 
 Vue.use(Vuex); // 把 store 绑到 Vue.prototype.$store = store
 
@@ -41,7 +42,8 @@ const store = new Vuex.Store({
       }
       if (index >= 0) {
         state.tagList.splice(index, 1);
-        store.commit('saveTags');
+        // store.commit('saveTags');
+        axios.delete(`https://622f04d73ff58f023c134e48.mockapi.io/tuanzi/tagList/${id}`).then(request => console.log('request', request.data));
         window.alert('已删除');
       } else {
         window.alert('删除失败');
@@ -61,23 +63,12 @@ const store = new Vuex.Store({
         JSON.stringify(state.recordList));
     },
     fetchTags(state) {
-      const initialTags = [
-        {name: '吃喝', type: '-'}, {name: '交通', type: '-'},
-        {name: '买菜', type: '-'}, {name: '服饰鞋包', type: '-'},
-        {name: '日用品', type: '-'}, {name: '零食水果', type: '-'},
-        {name: '超市', type: '-'}, {name: '红包', type: '-'},
-        {name: '话费', type: '-'}, {name: '娱乐', type: '-'},
-        {name: '医疗', type: '-'}, {name: '养车', type: '-'},
-        {name: '网费', type: '-'}, {name: '学习', type: '-'},
-        {name: '数码', type: '-'}, {name: '水电', type: '-'},
-        {name: '房租', type: '-'}, {name: '工资', type: '+'},
-        {name: '投资', type: '+'}, {name: '奖金', type: '+'},
-        {name: '兼职', type: '+'}, {name: '收款', type: '+'},
-        {name: '补贴', type: '+'}, {name: '生活费', type: '+'}];
-      state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
-      if (!state.tagList || state.tagList.length === 0) {
-        initialTags.map(tag => store.commit('createTag', tag));
-      }
+      axios.get(`https://622f04d73ff58f023c134e48.mockapi.io/tuanzi/tagList`).then(response => {state.tagList = response.data;});
+      setTimeout(() => {
+        console.log('state.tagList', state.tagList);
+        console.log('stringify(state.tagList)', JSON.stringify(state.tagList));
+      }, 5000);
+      window.localStorage.setItem('_idMax', '23');
     },
     createTag(state, tag: { name: string, type: string }) {
       state.createTagError = null;
@@ -87,13 +78,29 @@ const store = new Vuex.Store({
         return;
       }
       const id = createId().toString();
-      state.tagList.push({id, name: tag.name, type: tag.type});
-      store.commit('saveTags');
+      const newTag = {id, name: tag.name, type: tag.type};
+      state.tagList.push(newTag);
+      // store.commit('saveTags');
+      axios.post(`https://622f04d73ff58f023c134e48.mockapi.io/tuanzi/tagList/`, newTag).then(request => console.log('request', request.data));
     },
-    saveTags(state) {
-      window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
-    },
-  }
+    // saveTags(state) {
+    //   const initialTags = [
+    //     {name: '吃喝', type: '-'}, {name: '交通', type: '-'},
+    //     {name: '买菜', type: '-'}, {name: '服饰鞋包', type: '-'},
+    //     {name: '日用品', type: '-'}, {name: '零食水果', type: '-'},
+    //     {name: '超市', type: '-'}, {name: '红包', type: '-'},
+    //     {name: '话费', type: '-'}, {name: '娱乐', type: '-'},
+    //     {name: '医疗', type: '-'}, {name: '养车', type: '-'},
+    //     {name: '网费', type: '-'}, {name: '学习', type: '-'},
+    //     {name: '数码', type: '-'}, {name: '水电', type: '-'},
+    //     {name: '房租', type: '-'}, {name: '工资', type: '+'},
+    //     {name: '投资', type: '+'}, {name: '奖金', type: '+'},
+    //     {name: '兼职', type: '+'}, {name: '收款', type: '+'},
+    //     {name: '补贴', type: '+'}, {name: '生活费', type: '+'}];
+    //   const newTagList = initialTags.map(tag => store.commit('createTag', tag));
+    //   axios.put('https://622f04d73ff58f023c134e48.mockapi.io/tuanzi/tagList/:id', ...state.tagList).then(request => console.log('request',request.data));
+    // },
+  },
 });
 
 export default store;
